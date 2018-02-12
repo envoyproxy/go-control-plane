db98a4f0c731c7a3aa6505d4130803148dad261d
Kuat Yessenov <kuat@google.com>
2018-02-12 10:11:58 -0800

    Update data-plane-api
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- envoy/api/v2/core/address.pb.go
+++ envoy/api/v2/core/address.pb.go
@@ -84,7 +84,10 @@ func (SocketAddress_Protocol) EnumDescriptor() ([]byte, []int) {
 }
 
 type Pipe struct {
-	// Unix Domain Socket path.
+	// Unix Domain Socket path. On Linux, paths starting with '@' will use the
+	// abstract namespace. The starting '@' is replaced by a null byte by Envoy.
+	// Paths starting with '@' will result in an error in environments other than
+	// Linux.
 	Path string `protobuf:"bytes,1,opt,name=path,proto3" json:"path,omitempty"`
 }
 
