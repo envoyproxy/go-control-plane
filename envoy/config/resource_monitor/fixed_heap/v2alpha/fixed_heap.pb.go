5f1c666fa3aaa02d95e4fdd95e725f0c5eb7239f
lei-tang <32078630+lei-tang@users.noreply.github.com>
2018-08-08 10:23:57 -0700

    Update sha of data-plane-api and golang/x/sys/unix (#100)
    Signed-off-by: Lei Tang <32078630+lei-tang@users.noreply.github.com>

--- envoy/config/resource_monitor/fixed_heap/v2alpha/fixed_heap.pb.go
+++ envoy/config/resource_monitor/fixed_heap/v2alpha/fixed_heap.pb.go
@@ -20,9 +20,10 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
 
+// The fixed heap resource monitor reports the Envoy process memory pressure, computed as a
+// fraction of currently reserved heap memory divided by a statically configured maximum
+// specified in the FixedHeapConfig.
 type FixedHeapConfig struct {
-	// Limit of the Envoy process heap size. This is used to calculate heap memory pressure which
-	// is defined as (current heap size)/max_heap_size_bytes.
 	MaxHeapSizeBytes     uint64   `protobuf:"varint,1,opt,name=max_heap_size_bytes,json=maxHeapSizeBytes,proto3" json:"max_heap_size_bytes,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -33,7 +34,7 @@ func (m *FixedHeapConfig) Reset()         { *m = FixedHeapConfig{} }
 func (m *FixedHeapConfig) String() string { return proto.CompactTextString(m) }
 func (*FixedHeapConfig) ProtoMessage()    {}
 func (*FixedHeapConfig) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fixed_heap_6d52003d72b68727, []int{0}
+	return fileDescriptor_fixed_heap_5791aa3ac2da2b5a, []int{0}
 }
 func (m *FixedHeapConfig) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -308,10 +309,10 @@ var (
 )
 
 func init() {
-	proto.RegisterFile("envoy/config/resource_monitor/fixed_heap/v2alpha/fixed_heap.proto", fileDescriptor_fixed_heap_6d52003d72b68727)
+	proto.RegisterFile("envoy/config/resource_monitor/fixed_heap/v2alpha/fixed_heap.proto", fileDescriptor_fixed_heap_5791aa3ac2da2b5a)
 }
 
-var fileDescriptor_fixed_heap_6d52003d72b68727 = []byte{
+var fileDescriptor_fixed_heap_5791aa3ac2da2b5a = []byte{
 	// 176 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x72, 0x4c, 0xcd, 0x2b, 0xcb,
 	0xaf, 0xd4, 0x4f, 0xce, 0xcf, 0x4b, 0xcb, 0x4c, 0xd7, 0x2f, 0x4a, 0x2d, 0xce, 0x2f, 0x2d, 0x4a,
