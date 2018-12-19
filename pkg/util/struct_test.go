ecceb397a133b8a765e2ab764d50ede05449dc5a
Vadim Eisenberg <vadime@il.ibm.com>
2018-12-19 19:01:14 +0200

    fix formatting
    Signed-off-by: Vadim Eisenberg <vadime@il.ibm.com>

--- pkg/util/struct_test.go
+++ pkg/util/struct_test.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/gogo/protobuf/types"
 
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	"github.com/envoyproxy/go-control-plane/pkg/util"
 )
