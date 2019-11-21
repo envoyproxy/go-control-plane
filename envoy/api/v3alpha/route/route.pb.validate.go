28ff6b9a8779f47951e93a906b2df2af38946993
go-control-plane(CircleCI) <go-control-plane@users.noreply.github.com>
2019-11-21 05:49:55 +0000

    Mirrored from envoyproxy/envoy @ a907cff53f6ffb33d9a87b5ef50934626caa1b9e
    Signed-off-by: go-control-plane(CircleCI) <go-control-plane@users.noreply.github.com>

--- envoy/api/v3alpha/route/route.pb.validate.go
+++ envoy/api/v3alpha/route/route.pb.validate.go
@@ -269,6 +269,81 @@ var _ interface {
 	ErrorName() string
 } = VirtualHostValidationError{}
 
+// Validate checks the field values on FilterAction with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *FilterAction) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	if v, ok := interface{}(m.GetAction()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return FilterActionValidationError{
+				field:  "Action",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	return nil
+}
+
+// FilterActionValidationError is the validation error returned by
+// FilterAction.Validate if the designated constraints aren't met.
+type FilterActionValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e FilterActionValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e FilterActionValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e FilterActionValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e FilterActionValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e FilterActionValidationError) ErrorName() string { return "FilterActionValidationError" }
+
+// Error satisfies the builtin error interface
+func (e FilterActionValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sFilterAction.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = FilterActionValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = FilterActionValidationError{}
+
 // Validate checks the field values on Route with the rules defined in the
 // proto definition for this message. If any rules are violated, an error is returned.
 func (m *Route) Validate() error {
@@ -428,6 +503,18 @@ func (m *Route) Validate() error {
 			if err := v.Validate(); err != nil {
 				return RouteValidationError{
 					field:  "DirectResponse",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	case *Route_FilterAction:
+
+		if v, ok := interface{}(m.GetFilterAction()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return RouteValidationError{
+					field:  "FilterAction",
 					reason: "embedded message failed validation",
 					cause:  err,
 				}
