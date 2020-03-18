18b56d8e0a0e842410e1ca781a4459710fc48404
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-03-18 13:18:08 -0700

    fix some refs
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

--- pkg/cache/v2/resource_test.go
+++ pkg/cache/v2/resource_test.go
@@ -20,7 +20,7 @@ import (
 
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	v2route "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
-	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	cache "github.com/envoyproxy/go-control-plane/pkg/cache/v2"
 	"github.com/envoyproxy/go-control-plane/pkg/test/resource"
 )
 
--- pkg/cache/v2/simple_test.go
+++ pkg/cache/v2/simple_test.go
@@ -23,7 +23,7 @@ import (
 
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
-	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	cache "github.com/envoyproxy/go-control-plane/pkg/cache/v2"
 	"github.com/envoyproxy/go-control-plane/pkg/test/resource"
 )
 
