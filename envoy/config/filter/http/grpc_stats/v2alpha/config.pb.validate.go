acb5f601e33ec33ddadd608fd959b14a2d2f9df6
go-control-plane(CircleCI) <go-control-plane@users.noreply.github.com>
2020-03-27 00:13:56 +0000

    Mirrored from envoyproxy/envoy @ 715992f3ac891d87e67566ffbfd6b3af8d8a4e1d
    Signed-off-by: go-control-plane(CircleCI) <go-control-plane@users.noreply.github.com>

--- envoy/config/filter/http/grpc_stats/v2alpha/config.pb.validate.go
+++ envoy/config/filter/http/grpc_stats/v2alpha/config.pb.validate.go
@@ -46,6 +46,34 @@ func (m *FilterConfig) Validate() error {
 
 	// no validation rules for EmitFilterState
 
+	switch m.PerMethodStatSpecifier.(type) {
+
+	case *FilterConfig_IndividualMethodStatsAllowlist:
+
+		if v, ok := interface{}(m.GetIndividualMethodStatsAllowlist()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return FilterConfigValidationError{
+					field:  "IndividualMethodStatsAllowlist",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	case *FilterConfig_StatsForAllMethods:
+
+		if v, ok := interface{}(m.GetStatsForAllMethods()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return FilterConfigValidationError{
+					field:  "StatsForAllMethods",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	return nil
 }
 
