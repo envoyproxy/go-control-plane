90d10acf4413c0e5bededd1851a2577e10266eb0
Alec Holmes <alecholmez@me.com>
2020-05-12 16:15:04 -0400

    updated fork

--- envoy/extensions/filters/http/compressor/v3/compressor.pb.go
+++ envoy/extensions/filters/http/compressor/v3/compressor.pb.go
@@ -5,13 +5,14 @@ package envoy_extensions_filters_http_compressor_v3
 
 import (
 	fmt "fmt"
+	math "math"
+
 	_ "github.com/cncf/udpa/go/udpa/annotations"
 	v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	_ "github.com/envoyproxy/protoc-gen-validate/validate"
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/ptypes/any"
 	wrappers "github.com/golang/protobuf/ptypes/wrappers"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
