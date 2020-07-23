c72beae21940874da3bb6eac6a2bb23773de8286
go-control-plane(CircleCI) <go-control-plane@users.noreply.github.com>
2020-07-23 21:55:48 +0000

    Mirrored from envoyproxy/envoy @ e8216a8cf79c54e3e0a77ab729ebf27f4e79eb1b
    Signed-off-by: go-control-plane(CircleCI) <go-control-plane@users.noreply.github.com>

--- envoy/config/accesslog/v3/accesslog.pb.validate.go
+++ envoy/config/accesslog/v3/accesslog.pb.validate.go
@@ -1077,7 +1077,7 @@ func (m *ResponseFlagFilter) Validate() error {
 		if _, ok := _ResponseFlagFilter_Flags_InLookup[item]; !ok {
 			return ResponseFlagFilterValidationError{
 				field:  fmt.Sprintf("Flags[%v]", idx),
-				reason: "value must be in list [LH UH UT LR UR UF UC UO NR DI FI RL UAEX RLSE DC URX SI IH DPE UMSDR RFCF]",
+				reason: "value must be in list [LH UH UT LR UR UF UC UO NR DI FI RL UAEX RLSE DC URX SI IH DPE UMSDR RFCF NFCF]",
 			}
 		}
 
@@ -1164,6 +1164,7 @@ var _ResponseFlagFilter_Flags_InLookup = map[string]struct{}{
 	"DPE":   {},
 	"UMSDR": {},
 	"RFCF":  {},
+	"NFCF":  {},
 }
 
 // Validate checks the field values on GrpcStatusFilter with the rules defined
