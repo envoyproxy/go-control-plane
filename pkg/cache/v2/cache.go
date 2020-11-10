515fec7d7942847313af6a73a8c7b80ec51c4854
Kuat <kyessenov@users.noreply.github.com>
2020-11-10 15:25:02 -0800

    api: add custom buffer size for ADS, docs (#363)
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- pkg/cache/v2/cache.go
+++ pkg/cache/v2/cache.go
@@ -37,10 +37,10 @@ type ConfigWatcher interface {
 	// CreateWatch returns a new open watch from a non-empty request.
 	// An individual consumer normally issues a single open watch by each type URL.
 	//
-	// Once the requested resources are available, they should be pushed into responses channel.
-	// If there are any failures - nil should be written into the responses channel and the consumer should close the
-	// corresponding stream.
-	// Cache implementation should not close responses channel.
+	// Once the requested resources are available, they should be pushed into
+	// responses channel. If there are any failures - nil should be written into
+	// the responses channel and the server should close the corresponding
+	// stream. Cache implementation should not close the responses channel.
 	//
 	// Cancel is an optional function to release resources in the producer. If
 	// provided, the consumer may call this function multiple times.
