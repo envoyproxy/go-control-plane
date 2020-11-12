1d4e575d8b64f7649d1a421e01b86c6f05f8984c
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2020-11-12 03:07:41 +0000

    Mirrored from envoyproxy/envoy @ 4a14f1ce2dd92db2eb5673cf3cac4ebc9c9796b7
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/config/common/matcher/v4alpha/matcher.pb.go
+++ envoy/config/common/matcher/v4alpha/matcher.pb.go
@@ -35,6 +35,9 @@ const _ = proto.ProtoPackageIsVersion4
 // is found the action specified by the most specific on_no_match will be evaluated.
 // As an on_no_match might result in another matching tree being evaluated, this process
 // might repeat several times until the final OnMatch (or no match) is decided.
+//
+// This API is a work in progress.
+// [#not-implemented-hide:]
 type Matcher struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
