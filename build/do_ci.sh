bae3913cb311f97407901640bc85d02bad74d85f
Matt Klein <mklein@lyft.com>
2021-04-05 21:56:38 -0700

    fix
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- build/do_ci.sh
+++ build/do_ci.sh
@@ -3,7 +3,15 @@
 set -e
 set -x
 
+go install golang.org/x/tools/cmd/goimports@latest
+
 cd /go-control-plane
+
+NUM_DIFF_LINES=`/go/bin/goimports -local github.com/envoyproxy/go-control-plane -d pkg | wc -l`
+if [[ $NUM_DIFF_LINES > 0 ]]; then
+  echo "Failed format check. Run make format"
+  exit 1
+fi
 
 make create_version
 make check_version_dirty
