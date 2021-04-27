a880b87591e5a8b0a3e9f74d26ecc9b72b9c483d
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2021-04-27 12:58:58 +0000

    Mirrored from envoyproxy/envoy @ d20dbc4be73d2e185ecc3a8159ca95150e204aaa
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/extensions/filters/network/http_connection_manager/v3/http_connection_manager.pb.validate.go
+++ envoy/extensions/filters/network/http_connection_manager/v3/http_connection_manager.pb.validate.go
@@ -146,10 +146,10 @@ func (m *HttpConnectionManager) Validate() error {
 
 	if wrapper := m.GetMaxRequestHeadersKb(); wrapper != nil {
 
-		if val := wrapper.GetValue(); val <= 0 || val > 96 {
+		if val := wrapper.GetValue(); val <= 0 || val > 8192 {
 			return HttpConnectionManagerValidationError{
 				field:  "MaxRequestHeadersKb",
-				reason: "value must be inside range (0, 96]",
+				reason: "value must be inside range (0, 8192]",
 			}
 		}
 
