da39515e8f6ad108e5b5a44d6b40fc8fe2a80a0a
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2021-06-14 20:35:18 +0000

    Mirrored from envoyproxy/envoy @ 5e68aac3aaed1c431fdb1fcaad2a753f98d2362b
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/config/rbac/v3/rbac.pb.go
+++ envoy/config/rbac/v3/rbac.pb.go
@@ -89,9 +89,9 @@ func (RBAC_Action) EnumDescriptor() ([]byte, []int) {
 }
 
 // Role Based Access Control (RBAC) provides service-level and method-level access control for a
-// service. RBAC policies are additive. The policies are examined in order. Requests are allowed
-// or denied based on the `action` and whether a matching policy is found. For instance, if the
-// action is ALLOW and a matching policy is found the request should be allowed.
+// service. Requests are allowed or denied based on the `action` and whether a matching policy is
+// found. For instance, if the action is ALLOW and a matching policy is found the request should be
+// allowed.
 //
 // RBAC can also be used to make access logging decisions by communicating with access loggers
 // through dynamic metadata. When the action is LOG and at least one policy matches, the
@@ -155,6 +155,7 @@ type RBAC struct {
 	//
 	Action RBAC_Action `protobuf:"varint,1,opt,name=action,proto3,enum=envoy.config.rbac.v3.RBAC_Action" json:"action,omitempty"`
 	// Maps from policy name to policy. A match occurs when at least one policy matches the request.
+	// The policies are evaluated in lexicographic order of the policy name.
 	Policies map[string]*Policy `protobuf:"bytes,2,rep,name=policies,proto3" json:"policies,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
 }
 
