f200250f7ebfecc016731bc6bb465c0abdeb7f99
Snow Pettersen <snowp@lyft.com>
2021-07-30 09:29:17 -0400

    format
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- pkg/cache/v3/delta_test.go
+++ pkg/cache/v3/delta_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
@@ -14,7 +16,6 @@ import (
 	rsrc "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/server/stream/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/test/resource/v3"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSnapshotCacheDeltaWatch(t *testing.T) {
--- pkg/cache/v3/simple_test.go
+++ pkg/cache/v3/simple_test.go
@@ -22,13 +22,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	rsrc "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/test/resource/v3"
-	"github.com/stretchr/testify/assert"
 )
 
 type group struct{}
