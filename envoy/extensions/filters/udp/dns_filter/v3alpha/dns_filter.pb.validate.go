cf90f659a021bd505230c1c42e14413ee4a4fdf1
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2021-09-07 15:03:52 +0000

    Mirrored from envoyproxy/envoy @ ff9704e502184c4415d38351795f0d5d11627fcd
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/extensions/filters/udp/dns_filter/v3alpha/dns_filter.pb.validate.go
+++ envoy/extensions/filters/udp/dns_filter/v3alpha/dns_filter.pb.validate.go
@@ -256,6 +256,21 @@ func (m *DnsFilterConfig_ClientContextConfig) Validate() error {
 
 	}
 
+	for idx, item := range m.GetUpstreamResolvers() {
+		_, _ = idx, item
+
+		if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return DnsFilterConfig_ClientContextConfigValidationError{
+					field:  fmt.Sprintf("UpstreamResolvers[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if v, ok := interface{}(m.GetDnsResolutionConfig()).(interface{ Validate() error }); ok {
 		if err := v.Validate(); err != nil {
 			return DnsFilterConfig_ClientContextConfigValidationError{
