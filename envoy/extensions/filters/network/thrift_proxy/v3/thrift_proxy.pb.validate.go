e6111eeb18c1c6f9bd032fd402c6f03f8f9a26f5
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2022-03-15 04:20:53 +0000

    Mirrored from envoyproxy/envoy @ ec7ef59ef36ccb5e2a9b20493ae23673504d75b9
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/extensions/filters/network/thrift_proxy/v3/thrift_proxy.pb.validate.go
+++ envoy/extensions/filters/network/thrift_proxy/v3/thrift_proxy.pb.validate.go
@@ -35,6 +35,146 @@ var (
 	_ = sort.Sort
 )
 
+// Validate checks the field values on Trds with the rules defined in the proto
+// definition for this message. If any rules are violated, the first error
+// encountered is returned, or nil if there are no violations.
+func (m *Trds) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Trds with the rules defined in the
+// proto definition for this message. If any rules are violated, the result is
+// a list of violation errors wrapped in TrdsMultiError, or nil if none found.
+func (m *Trds) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Trds) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if m.GetConfigSource() == nil {
+		err := TrdsValidationError{
+			field:  "ConfigSource",
+			reason: "value is required",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if all {
+		switch v := interface{}(m.GetConfigSource()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, TrdsValidationError{
+					field:  "ConfigSource",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, TrdsValidationError{
+					field:  "ConfigSource",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetConfigSource()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return TrdsValidationError{
+				field:  "ConfigSource",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	// no validation rules for RouteConfigName
+
+	if len(errors) > 0 {
+		return TrdsMultiError(errors)
+	}
+
+	return nil
+}
+
+// TrdsMultiError is an error wrapping multiple validation errors returned by
+// Trds.ValidateAll() if the designated constraints aren't met.
+type TrdsMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m TrdsMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m TrdsMultiError) AllErrors() []error { return m }
+
+// TrdsValidationError is the validation error returned by Trds.Validate if the
+// designated constraints aren't met.
+type TrdsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TrdsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TrdsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TrdsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TrdsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TrdsValidationError) ErrorName() string { return "TrdsValidationError" }
+
+// Error satisfies the builtin error interface
+func (e TrdsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTrds.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TrdsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TrdsValidationError{}
+
 // Validate checks the field values on ThriftProxy with the rules defined in
 // the proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
@@ -113,6 +253,35 @@ func (m *ThriftProxy) validate(all bool) error {
 		if err := v.Validate(); err != nil {
 			return ThriftProxyValidationError{
 				field:  "RouteConfig",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetTrds()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ThriftProxyValidationError{
+					field:  "Trds",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ThriftProxyValidationError{
+					field:  "Trds",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetTrds()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ThriftProxyValidationError{
+				field:  "Trds",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
