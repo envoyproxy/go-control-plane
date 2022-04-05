d12adc5eba8e79653c4c9dd3b3ef4ace6e4d008e
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2022-04-05 15:52:44 +0000

    Mirrored from envoyproxy/envoy @ 90a1d4b76a221d347b8b534ec7c84971e3a48422
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/extensions/filters/udp/udp_proxy/v3/udp_proxy.pb.validate.go
+++ envoy/extensions/filters/udp/udp_proxy/v3/udp_proxy.pb.validate.go
@@ -224,6 +224,37 @@ func (m *UdpProxyConfig) validate(all bool) error {
 			errors = append(errors, err)
 		}
 
+	case *UdpProxyConfig_Matcher:
+
+		if all {
+			switch v := interface{}(m.GetMatcher()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, UdpProxyConfigValidationError{
+						field:  "Matcher",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, UdpProxyConfigValidationError{
+						field:  "Matcher",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetMatcher()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return UdpProxyConfigValidationError{
+					field:  "Matcher",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		err := UdpProxyConfigValidationError{
 			field:  "RouteSpecifier",
