983b508ca70d5be6b8727c716f7c9c6c580e0232
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2022-12-03 17:37:16 +0000

    Mirrored from envoyproxy/envoy @ 5784db4d1ecf0b4a971b74707544b6328e96848c
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/config/route/v3/route_components.pb.validate.go
+++ envoy/config/route/v3/route_components.pb.validate.go
@@ -825,6 +825,139 @@ var _ interface {
 	ErrorName() string
 } = FilterActionValidationError{}
 
+// Validate checks the field values on RouteList with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *RouteList) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on RouteList with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in RouteListMultiError, or nil
+// if none found.
+func (m *RouteList) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *RouteList) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	for idx, item := range m.GetRoutes() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, RouteListValidationError{
+						field:  fmt.Sprintf("Routes[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, RouteListValidationError{
+						field:  fmt.Sprintf("Routes[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return RouteListValidationError{
+					field:  fmt.Sprintf("Routes[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return RouteListMultiError(errors)
+	}
+
+	return nil
+}
+
+// RouteListMultiError is an error wrapping multiple validation errors returned
+// by RouteList.ValidateAll() if the designated constraints aren't met.
+type RouteListMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m RouteListMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m RouteListMultiError) AllErrors() []error { return m }
+
+// RouteListValidationError is the validation error returned by
+// RouteList.Validate if the designated constraints aren't met.
+type RouteListValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e RouteListValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e RouteListValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e RouteListValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e RouteListValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e RouteListValidationError) ErrorName() string { return "RouteListValidationError" }
+
+// Error satisfies the builtin error interface
+func (e RouteListValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sRouteList.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = RouteListValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = RouteListValidationError{}
+
 // Validate checks the field values on Route with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
