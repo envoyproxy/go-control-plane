5efe59dc39e4ec6624eefbfdbe58949bf9e2752c
Valerian Roche <valerian.roche@datadoghq.com>
2023-03-19 20:16:44 -0400

    Revert "Fix for the SDS update failure (#615)" as no longer needed on top of #559 (#657)
    * Set safe directory in CI script to fix build issue related to VCS stamping in binaries
    
    Signed-off-by: Valerian Roche <valerian.roche@datadoghq.com>
    
    * Revert "Fix for the SDS update failure (#615)"
    
    This reverts commit 0e0f25d5c2eddbf109478c443d8dc856b36692e3.
    
    Signed-off-by: Valerian Roche <valerian.roche@datadoghq.com>

--- build/do_ci.sh
+++ build/do_ci.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+# Needed to avoid issues with go version stamping in CI build
+git config --global --add safe.directory /go-control-plane
+
 go install golang.org/x/tools/cmd/goimports@latest
 
 cd /go-control-plane
