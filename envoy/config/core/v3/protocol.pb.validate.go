9050ddd7297e33961e941bc5948c7341fceb2ca5
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2023-04-11 01:04:03 +0000

    Mirrored from envoyproxy/envoy @ 73115db835f27a79a79a44b8ee8f7ac4333891c7
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/config/core/v3/protocol.pb.validate.go
+++ envoy/config/core/v3/protocol.pb.validate.go
@@ -1800,6 +1800,35 @@ func (m *Http2ProtocolOptions) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetUseOghttp2Codec()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, Http2ProtocolOptionsValidationError{
+					field:  "UseOghttp2Codec",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, Http2ProtocolOptionsValidationError{
+					field:  "UseOghttp2Codec",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetUseOghttp2Codec()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return Http2ProtocolOptionsValidationError{
+				field:  "UseOghttp2Codec",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return Http2ProtocolOptionsMultiError(errors)
 	}
