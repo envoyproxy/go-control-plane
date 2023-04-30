36f68c9d6c49e4c964ca79eaac8e7d4db7078046
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2023-04-30 20:22:13 +0000

    Mirrored from envoyproxy/envoy @ fe274c0277a8633b277f02b720930137280f7529
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/config/rbac/v3/rbac.pb.go
+++ envoy/config/rbac/v3/rbac.pb.go
@@ -799,6 +799,11 @@ type Principal_SourceIp struct {
 	// A CIDR block that describes the downstream IP.
 	// This address will honor proxy protocol, but will not honor XFF.
 	//
+	// This field is deprecated; either use :ref:`direct_remote_ip
+	// <envoy_v3_api_field_config.rbac.v3.Principal.direct_remote_ip>` for the same
+	// behavior, or use
+	// :ref:`remote_ip <envoy_v3_api_field_config.rbac.v3.Principal.remote_ip>`.
+	//
 	// Deprecated: Do not use.
 	SourceIp *v32.CidrRange `protobuf:"bytes,5,opt,name=source_ip,json=sourceIp,proto3,oneof"`
 }
