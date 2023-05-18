13eae49fea464cb603bb33d67c55e1c139010498
go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>
2023-05-18 04:56:19 +0000

    Mirrored from envoyproxy/envoy @ 7d1b1d0040054093575dcda742099be9641e3a64
    Signed-off-by: go-control-plane(Azure Pipelines) <go-control-plane@users.noreply.github.com>

--- envoy/extensions/filters/http/ext_proc/v3/ext_proc.pb.validate.go
+++ envoy/extensions/filters/http/ext_proc/v3/ext_proc.pb.validate.go
@@ -225,6 +225,35 @@ func (m *ExternalProcessor) validate(all bool) error {
 
 	// no validation rules for DisableClearRouteCache
 
+	if all {
+		switch v := interface{}(m.GetForwardRules()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ExternalProcessorValidationError{
+					field:  "ForwardRules",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ExternalProcessorValidationError{
+					field:  "ForwardRules",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetForwardRules()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ExternalProcessorValidationError{
+				field:  "ForwardRules",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return ExternalProcessorMultiError(errors)
 	}
@@ -304,6 +333,137 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = ExternalProcessorValidationError{}
+
+// Validate checks the field values on HeaderForwardingRules with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *HeaderForwardingRules) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on HeaderForwardingRules with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// HeaderForwardingRulesMultiError, or nil if none found.
+func (m *HeaderForwardingRules) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *HeaderForwardingRules) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if all {
+		switch v := interface{}(m.GetAllowedHeaders()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, HeaderForwardingRulesValidationError{
+					field:  "AllowedHeaders",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, HeaderForwardingRulesValidationError{
+					field:  "AllowedHeaders",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAllowedHeaders()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return HeaderForwardingRulesValidationError{
+				field:  "AllowedHeaders",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if len(errors) > 0 {
+		return HeaderForwardingRulesMultiError(errors)
+	}
+
+	return nil
+}
+
+// HeaderForwardingRulesMultiError is an error wrapping multiple validation
+// errors returned by HeaderForwardingRules.ValidateAll() if the designated
+// constraints aren't met.
+type HeaderForwardingRulesMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m HeaderForwardingRulesMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m HeaderForwardingRulesMultiError) AllErrors() []error { return m }
+
+// HeaderForwardingRulesValidationError is the validation error returned by
+// HeaderForwardingRules.Validate if the designated constraints aren't met.
+type HeaderForwardingRulesValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e HeaderForwardingRulesValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e HeaderForwardingRulesValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e HeaderForwardingRulesValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e HeaderForwardingRulesValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e HeaderForwardingRulesValidationError) ErrorName() string {
+	return "HeaderForwardingRulesValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e HeaderForwardingRulesValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sHeaderForwardingRules.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = HeaderForwardingRulesValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = HeaderForwardingRulesValidationError{}
 
 // Validate checks the field values on ExtProcPerRoute with the rules defined
 // in the proto definition for this message. If any rules are violated, the
