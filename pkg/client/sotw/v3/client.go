a98f8ef069ae54183394031a2af77244a38adb34
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-10-05 07:50:18 +0200

    deps: use google.golang.org/protobuf instead of github.com/golang/protobuf
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>
    
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- pkg/client/sotw/v3/client.go
+++ pkg/client/sotw/v3/client.go
@@ -21,11 +21,12 @@ import (
 	"io"
 	"sync"
 
-	"github.com/golang/protobuf/ptypes/any"
 	status "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	grpcStatus "google.golang.org/grpc/status"
+
+	"github.com/golang/protobuf/ptypes/any"
 
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
