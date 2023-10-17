de9f49cc796affdee514e912fdbf966245a832cc
huabing zhao <zhaohuabing@gmail.com>
2023-10-17 16:18:37 +0800

    correct the ordering comment
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- pkg/cache/v3/order_test.go
+++ pkg/cache/v3/order_test.go
@@ -64,9 +64,9 @@ func TestOrderKeys(t *testing.T) {
 
 	// Ordering:
 	// === RUN   TestOrderKeys
+	// order_test.go:43: {ID:2 TypeURL:type.googleapis.com/envoy.config.cluster.v3.Cluster}
+	// order_test.go:43: {ID:1 TypeURL:type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment}
 	// order_test.go:43: {ID:4 TypeURL:type.googleapis.com/envoy.config.listener.v3.Listener}
 	// order_test.go:43: {ID:3 TypeURL:type.googleapis.com/envoy.config.route.v3.RouteConfiguration}
 	// order_test.go:43: {ID:5 TypeURL:type.googleapis.com/envoy.config.route.v3.ScopedRouteConfiguration}
-	// order_test.go:43: {ID:2 TypeURL:type.googleapis.com/envoy.config.cluster.v3.Cluster}
-	// order_test.go:43: {ID:1 TypeURL:type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment}
 }
